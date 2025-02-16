76f077ed7274847c424e53bcb08e9c3d73618988
Jesús Antonio Gil <jesus.antoniogil@telefonica.com>
2025-02-16 19:01:33 +0100

    Create project

---
+++ RippleEffect/RippleEffect/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RippleEffect
+//
+//  Created by Jesus Antonio Gil on 16/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RippleEffect/RippleEffect/RippleEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  RippleEffectApp.swift
+//  RippleEffect
+//
+//  Created by Jesus Antonio Gil on 16/2/25.
+//
+
+import SwiftUI
+
+@main
+struct RippleEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
