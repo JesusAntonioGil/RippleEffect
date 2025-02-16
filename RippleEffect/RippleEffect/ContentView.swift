80849382b5ad3a8f6a5fb62485ef95730d68c4d4
Jesús Antonio Gil <jesus.antoniogil@telefonica.com>
2025-02-16 19:19:02 +0100

    Ripple effect done

--- RippleEffect/RippleEffect/ContentView.swift
+++ RippleEffect/RippleEffect/ContentView.swift
@@ -7,17 +7,59 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State private var ripple = Ripple(diameter: 10, isFadding: false)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            RippleView(ripple: $ripple)
+                .opacity(ripple.isFadding ? 0.1 : 1)
+            
+            Rectangle()
+                .fill(.clear)
         }
-        .padding()
+        .onAppear {
+            withAnimation(.linear(duration: 3.0).repeatForever(autoreverses: false)) {
+                ripple.isFadding = true
+                ripple.diameter = 300
+            }
+        }
+        .background(Color.init(red: 229/255, green: 229/255, blue: 239/255))
+        .edgesIgnoringSafeArea(.all)
     }
 }
+
+
+struct Ripple {
+    var diameter: CGFloat
+    var isFadding: Bool
+}
+
+
+struct RippleView: View {
+    @Binding var ripple: Ripple
+    
+    
+    var body: some View {
+        Circle()
+            .fill(Color.init(red: 225/255, green: 225/255, blue: 235/255))
+            .frame(width: ripple.diameter, height: ripple.diameter)
+            .overlay {
+                Circle()
+                    .stroke(.gray, lineWidth: 10)
+                    .blur(radius: 4)
+                    .offset(x: 2, y: 2)
+            }
+            .overlay {
+                Circle()
+                    .stroke(.white, lineWidth: 10)
+                    .blur(radius: 4)
+                    .offset(x: -2, y: -2)
+            }
+    }
+}
+
 
 #Preview {
     ContentView()
